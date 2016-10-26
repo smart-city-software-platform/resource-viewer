12b59a55630e8914d005994be11607a38c9f596b
Thiago Petrone <thiago.petrone@gmail.com>
2016-10-26 17:26:04 -0200

    Test data-collector pulling (with mock)
    Signed-off-by: Thiago Petrone <thiago.petrone@gmail.com>

---
+++ server/mocks/infos.js
@@ -0,0 +1,78 @@
+/*jshint node:true*/
+module.exports = function(app) {
+  var express = require('express');
+  var infosRouter = express.Router();
+
+  infosRouter.get('/', function(req, res) {
+    res.send({
+      'infos': [
+      {
+        "id": 3,
+        "uuid": "ae9cf502-5ed2-47d4-914c-c1caec1c41c4",
+        "capabilities": {
+          "Temperature": [
+          {
+            "value": "28.237",
+            "date": "2016-06-20T06:37:52.000Z"
+          }],
+          "Humidity": [
+          {
+            "value": "28.247",
+            "date": "2016-06-20T06:37:52.000Z"
+          }]
+        }
+      }]
+    });
+  });
+
+  infosRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  infosRouter.get('/:id', function(req, res) {
+    res.send({
+      'infos': {
+        // id: req.params.id,
+        id: 0,
+        "uuid": "ae9cf502-5ed2-47d4-914c-c1caec1c41c4",
+        "capabilities": {
+          "Temperature": [
+          {
+            "value": "28.237",
+            "date": "2016-06-20T06:37:52.000Z"
+          }],
+          "Humidity": [
+          {
+            "value": "28.247",
+            "date": "2016-06-20T06:37:52.000Z"
+          }]
+        }
+
+      }
+    });
+  });
+
+  infosRouter.put('/:id', function(req, res) {
+    res.send({
+      'infos': {
+        id: req.params.id
+      }
+    });
+  });
+
+  infosRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  // The POST and PUT call will not contain a request body
+  // because the body-parser is not included by default.
+  // To use req.body, run:
+
+  //    npm install --save-dev body-parser
+
+  // After installing, you need to `use` the body-parser for
+  // this mock uncommenting the following line:
+  //
+  //app.use('/api/infos', require('body-parser').json());
+  app.use('/api/infos', infosRouter);
+};
