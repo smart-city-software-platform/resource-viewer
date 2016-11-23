0a938bdf6522d0a49629b27f7375d75ec2a76d13
Thiago Petrone <thiago.petrone@gmail.com>
2016-11-23 16:24:58 -0200

    Fix tests
    Signed-off-by: Ariel Palmeira <arielpalmeiracontato@gmail.com>
    Signed-off-by: Thiago Petrone <thiago.petrone@gmail.com>

--- tests/acceptance/home-test.js
+++ tests/acceptance/home-test.js
@@ -7,7 +7,7 @@ test('visiting /', function(assert) {
   visit('/');
 
   andThen(function() {
-    assert.equal(currentURL(), '/');
+    assert.equal(currentURL(), '/resources');
     assert.equal(find('h1').text(), 'Resources');
     assert.equal(find('th').first().text(), 'ID');
     assert.equal(find('th').get(1).innerHTML, 'Description');
--- tests/unit/controllers/resources/index-test.js
+++ tests/unit/controllers/resources/index-test.js
@@ -1,12 +1,25 @@
 import { moduleFor, test } from 'ember-qunit';
+import wait from 'ember-test-helpers/wait';
 
 moduleFor('controller:resources/index', 'Unit | Controller | resources/index', {
   // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ['adapter:info']
 });
 
 // Replace this with your real tests.
 test('it exists', function(assert) {
   let controller = this.subject();
   assert.ok(controller);
+});
+
+test('updateUuid', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+
+  controller.send('updateUuid', 'ae9cf502-5ed2-47d4-914c-c1caec1c41c4');
+
+  return wait().then(() => {
+    assert.equal(controller.get('capability').Temperature[0].value, 28.237);
+  });
+
 });
